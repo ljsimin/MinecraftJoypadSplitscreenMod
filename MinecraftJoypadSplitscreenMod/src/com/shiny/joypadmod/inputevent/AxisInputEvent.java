ac5937a1fabbb6f998088633d930471552cb961e
Andrew Hickey <andy_hickey@telus.net>
2014-03-02 13:18:05 -0800

    Fix a couple of binding bugs found from non standard joypads.

--- MinecraftJoypadSplitscreenMod/src/com/shiny/joypadmod/inputevent/AxisInputEvent.java
+++ MinecraftJoypadSplitscreenMod/src/com/shiny/joypadmod/inputevent/AxisInputEvent.java
@@ -2,6 +2,8 @@ package com.shiny.joypadmod.inputevent;
 
 import org.lwjgl.input.Controllers;
 
+import com.shiny.joypadmod.helpers.LogHelper;
+
 public class AxisInputEvent extends ControllerInputEvent
 {
 	int axisNumber;
@@ -13,7 +15,18 @@ public class AxisInputEvent extends ControllerInputEvent
 		// check if valid axis
 		super(EventType.AXIS, controllerId, axisNumber, threshold, deadzone);
 		this.axisNumber = axisNumber;
-		this.setDeadZone(deadzone);
+		if (isValid())
+		{
+			this.setDeadZone(deadzone);
+		}
+		else
+		{
+			LogHelper.Error("Attempted to create a binding with invalid axis number. Axis index requested: "
+					+ axisNumber + " axis available: " + Controllers.getController(controllerNumber).getAxisCount());
+			LogHelper.Warn("Processing will continue with invalid axis " + axisNumber
+					+ ".  Binding will not respond until rebound and may cause instability in mod.");
+		}
+
 	}
 
 	@Override
@@ -25,24 +38,32 @@ public class AxisInputEvent extends ControllerInputEvent
 	@Override
 	public float getAnalogReading()
 	{
+		if (!isValid())
+			return 0;
 		return Controllers.getController(controllerNumber).getAxisValue(axisNumber);
 	}
 
 	@Override
 	public float getDeadZone()
 	{
+		if (!isValid())
+			return 0;
 		return Controllers.getController(controllerNumber).getDeadZone(axisNumber);
 	}
 
 	@Override
 	public String getName()
 	{
+		if (!isValid())
+			return "Invalid";
 		return Controllers.getController(controllerNumber).getAxisName(axisNumber);
 	}
 
 	@Override
 	public String toString()
 	{
+		if (!isValid())
+			return "Invalid";
 		return new StringBuilder().append("Event: ").append(getName()).append(" Type: ").append(getEventType()).append(
 				" Threshold: ").append(threshold).append(" Current value: ").append(getAnalogReading()).append(
 				" Is pressed: ").append(isPressed()).toString();
@@ -51,6 +72,10 @@ public class AxisInputEvent extends ControllerInputEvent
 	@Override
 	public void setDeadZone(float deadzone)
 	{
+		if (!isValid())
+			return;
+		LogHelper.Info("Setting deadzone on controller " + controllerNumber + " axis " + this.axisNumber + " value "
+				+ deadzone);
 		Controllers.getController(controllerNumber).setDeadZone(this.axisNumber, deadzone);
 		this.deadzone = deadzone;
 	}
@@ -58,6 +83,9 @@ public class AxisInputEvent extends ControllerInputEvent
 	@Override
 	public String getDescription()
 	{
+		if (!isValid())
+			return "Invalid";
+
 		return new StringBuilder().append(getName()).append(" ").append(getDirection(getThreshold())).toString();
 	}
 
