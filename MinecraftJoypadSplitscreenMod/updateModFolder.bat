c07e25cd3d8f002f8752d3f9f362bf58b4cda3af
Andrew Hickey <andy_hickey@telus.net>
2014-03-02 23:52:50 -0800

    Update build.gradle / helper batch

--- MinecraftJoypadSplitscreenMod/updateModFolder.bat
+++ MinecraftJoypadSplitscreenMod/updateModFolder.bat
@@ -1,3 +1,4 @@
 REM after building using gradle, run this batch to copy the mod to your minecraft mod folder for testing
-erase %APPDATA%\.minecraft\mods\joypadsplit*
-copy build\libs\* %APPDATA%\.minecraft\mods\joypadsplit*
+erase %APPDATA%\.minecraft\mods\JoypadMod-1.7.2*
+copy build\libs\* %APPDATA%\.minecraft\mods\
+erase build\libs\*
