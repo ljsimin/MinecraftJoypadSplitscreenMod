d3e567d8b408dffa0b1b954a7bc15273184b7e2f
ahick <ahick@Mini-Horse>
2016-12-06 22:54:55 -0800

    Remove stuff as we will import it from github

--- MinecraftJoypadSplitscreenMod/src/main/java/com/ivan/xinput/exceptions/XInputNotLoadedException.java
+++
@@ -1,9 +0,0 @@
-package com.ivan.xinput.exceptions;
-
-public class XInputNotLoadedException extends Exception {
-    private static final long serialVersionUID = -2886184031373724836L;
-
-    public XInputNotLoadedException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-}
