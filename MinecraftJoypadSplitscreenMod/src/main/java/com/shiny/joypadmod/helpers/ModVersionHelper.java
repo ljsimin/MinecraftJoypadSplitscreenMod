f967f2dd8040e329093f3c85246e87e9831acf4e
ahick <ahick@Mini-Horse>
2016-12-11 00:16:17 -0800

    1.10.2 specific changes

--- MinecraftJoypadSplitscreenMod/src/main/java/com/shiny/joypadmod/helpers/ModVersionHelper.java
+++ MinecraftJoypadSplitscreenMod/src/main/java/com/shiny/joypadmod/helpers/ModVersionHelper.java
@@ -20,9 +20,8 @@ import net.minecraft.client.gui.ScaledResolution;
 
 public class ModVersionHelper
 {
-	public static final String VERSION = "1.10.0";
-	//what should I type there for 1.10?
-	public static final int MC_VERSION = 190;
+	public static final String VERSION = "1.10.2";
+	public static final int MC_VERSION = 1102;
 
 	public void gameInit()
 	{
