49952c870ea93e925fe5ee6aa89fd6554e580ca9
ahick <ahick@Mini-Horse>
2017-01-09 22:09:11 -0800

    Remove the battery level indicator test.

--- MinecraftJoypadSplitscreenMod/src/main/java/com/shiny/joypadmod/helpers/ModVersionHelper.java
+++ MinecraftJoypadSplitscreenMod/src/main/java/com/shiny/joypadmod/helpers/ModVersionHelper.java
@@ -21,7 +21,7 @@ import net.minecraft.client.gui.ScaledResolution;
 public class ModVersionHelper
 {
 	public static final String VERSION = "1.11.0";
-	//what should I type there for 1.10?
+	
 	public static final int MC_VERSION = 1110;
 
 	public void gameInit()
