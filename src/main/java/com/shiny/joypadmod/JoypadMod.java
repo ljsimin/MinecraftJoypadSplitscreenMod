f791fe6d095fc42aa3ed074d00ce5f82dc1269d1
ahickey <andy_hickey@telus.net>
2019-10-02 10:14:31 -0700

    Fix the mcmod author list for proper display in mod info.
    Change  modid back to old ID to allow Forge to detect if there are more than one version of the mod (like an older version) in mod folder.

--- src/main/java/com/shiny/joypadmod/JoypadMod.java
+++ src/main/java/com/shiny/joypadmod/JoypadMod.java
@@ -26,9 +26,9 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = JoypadMod.MODID, name = JoypadMod.NAME, version = JoypadMod.VERSION, clientSideOnly = true, acceptedMinecraftVersions = "[1.12]")
 public class JoypadMod {
     public static Logger logger = LogManager.getLogger("Joypad Mod");
-    public static final String MODID = "joypadmod";
+    public static final String MODID = "joypadsplitscreenmod";
     public static final String NAME = "Joypad Mod";
-    public static final String VERSION = "1.12.0-0.22";
+    public static final String VERSION = "1.12-14.21.0.2387";
 
     private static ControllerSettings controllerSettings;
 
